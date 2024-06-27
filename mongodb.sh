72be5f0d3a439c9f4d40fc618ea4eca0b4b1be13
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-06-27 21:46:12 +0530

    roboshop-shellscripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -9,21 +7,48 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+echo "script startted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ... $R FAILED $N"
+       echo -e"$2.....$R failed $N"
+    
     else
-        echo -e "$2 ... $G SUCCESS $N"
+        echo -e "$2....$G sucess $N"
+
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 # you can give other than 0
+    echo "error ;; please run the script with root acess"
+exit 1
 else
-    echo "You are root user"
-fi #
+     echo " you are root acess"
+fi
+
+cp mongo-repo /etc/yum.repo.d/devops &>> $LOGFILE
+  
+  VALIDATE $? "copied  mongodb Repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb"
+
+
+systemctl enable mongodb &>> $LOGFILE
+
+VALIDATE $? "enabling mongodb"
+
+systemctl start  mongod &>> $LOGFILE
+
+VALIDATE $? "starting mongodb"
+
+sed-i 's/ 127.0.0.1 to 0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "remote acess to  mongodb"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "restarting mongodb"
