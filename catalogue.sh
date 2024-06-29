21edce2c1d9406fc9a8b133cdaf8b80398594943
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-06-29 12:53:13 +0530

    catalogue service

---
+++ catalogue.sh
@@ -0,0 +1,88 @@
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? " disabling current nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? " enaabling current nodejs: 18"
+
+useradd roboshop &>> $LOGFILE
+
+VALIDATE $? "creating roboshop user"
+
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "Creating app directory"
+
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+
+VALIDATE $? "downloading the catalogue application"&>> $LOGFILE
+
+cd /app 
+
+unzip -o /tmp/catalogue.zip
+
+VALIDATE $? " unzipping catalogue"&>> $LOGFILE
+
+npm install
+
+VALIDATE $? " unzipping catalogue"&>> $LOGFILE
+
+cp /K.Durgaprasad/devops/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+
+VALIDATE $? " copying catalogue service file"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? " catalogue daemon reload"
+ 
+ systemctl enable catalogue &>> $LOGFILE
+
+
+ VALIDATE $? " Enable catalogue" 
+
+systemctl start catalogue &>> $LOGFILE
+
+VALIDATE $? "Starting catalogue"
+
+cp /K.Durgaprasad/devops/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB client"
+
+mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
+
+VALIDATE $? "Loading catalouge data into MongoDB"
