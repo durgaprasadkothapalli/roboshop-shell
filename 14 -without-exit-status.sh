3a3f2ddbe40116bc13f500108be245da918ea8da
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-07-04 16:07:23 +0530

    roboshop shell script

---
+++ 14 -without-exit-status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+ls -ltr
+
+touch example.txt
+
+echo "lslslslslslslsl"
+
+lsfff
+
+ cd /tmp
+
+ cd /home/centos
--- cataloguesiva.sh
+++ cataloguesiva.sh
@@ -42,7 +42,7 @@ dnf install nodejs -y  &>> $LOGFILE
 
 VALIDATE $? "Installing NodeJS:18"
 
-id roboshop #if roboshop user does not exist, then it is failure
+id roboshop &>> $LOGFILE#if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
 then
     useradd roboshop
