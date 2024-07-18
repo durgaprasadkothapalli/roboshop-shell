8da5ee896f9803352f447c08abf6e6b97597b13b
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-07-18 11:30:13 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -2,16 +2,16 @@
 
 AMI=ami-0b4f379183e5706b9 
 SG_ID=sg-085cac89efaca39c7 #replace with your id
-INSTANCES=( "mongodb" "redis" "mysql" "rabbitmq""catalogue" "user" "payment" "dispatch" "web")
+INSTANCES=( "mongodb" "redis" "mysql" "rabbitmq""catalogue" "user" "payment" "dispatch" "web" "cart" "shipping")
 
 for i in "${INSTANCES[@]}"
-  do
+do
       if  [ $i == "mongodb"] || [ $i == "mysql"] || [ $i == "shipping"] ||
-         then
+      then
            INSTANCES_TYPE="t3.small"
-         else
+       else
             INSTANCES_TYPE="t2.micro"
-fi
+       fi
 
         aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --INSTANCE_TYPE t2.micro --security-group-ids sg-085cac89efaca39c7
-done
+ done 
