69354931338ad6d0110f9eb65504f1424942f5ad
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-07-18 12:13:22 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -7,11 +7,11 @@ INSTANCES=( "mongodb" "redis" "mysql" "rabbitmq""catalogue" "user" "payment" "di
 for i in "${INSTANCES[@]}"
 do
       if  [ $i == "mongodb"] || [ $i == "mysql"] || [ $i == "shipping"] ||
-      
+      then
            INSTANCES_TYPE="t3.small"
        else
-            INSTANCES_TYPE="t2.micro"
+           INSTANCES_TYPE="t2.micro"
        fi
 
-        aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --INSTANCE_TYPE t2.micro --security-group-ids sg-085cac89efaca39c7
+        aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE t2.micro --security-group-ids sg-085cac89efaca39c7
  done 
