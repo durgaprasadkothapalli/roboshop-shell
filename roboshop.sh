21b8c4c077b8d7fdd501f95cdfc75c42ba6aa409
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-07-18 15:05:19 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-AMI=ami-0b4f379183e5706b9 
-SG_ID=sg-085cac89efaca39c7 #replace with your id
-INSTANCES=( "mongodb" "redis" "mysql" "rabbitmq""catalogue" "user" "payment" "dispatch" "web" "cart" "shipping")
+AMI=ami-0b4f379183e5706b9  #this keeps on changing
+SG_ID=sg-085cac89efaca39c7 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 for i in "${INSTANCES[@]}"
 do
-       echo " instance is : $i"
-      if [ $i == "mongodb"] || [ $i == "mysql"] || [ $i == "shipping"] ||
-        then
-           INSTANCES_TYPE="t3.small"
-        else
-           INSTANCES_TYPE="t2.micro"
-      fi
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
 
-        aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-085cac89efaca39c7
-done 
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-085cac89efaca39c7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+   
