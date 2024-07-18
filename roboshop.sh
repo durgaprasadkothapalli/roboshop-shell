035faf0a165576e790fffc51200c29b6f6578078
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-07-18 12:46:39 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -8,11 +8,11 @@ for i in "${INSTANCES[@]}"
 do
        echo " instance is : $i"
       if  [ $i == "mongodb"] || [ $i == "mysql"] || [ $i == "shipping"] ||
-      then
+        then
            INSTANCES_TYPE="t3.small"
-       else
+        else
            INSTANCES_TYPE="t2.micro"
-       fi
+      fi
 
         aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-085cac89efaca39c7
- done 
+done 
